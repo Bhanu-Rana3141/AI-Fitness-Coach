ece33254148ae46849a1f17478569f50445556c3
karan16kkg <karankumargarg1610@gmail.com>
2025-02-21 14:58:17 +0530

    change the frontend page

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <div className='border-2 border-red-500'>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact Us</li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Home
